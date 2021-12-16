ab6d1ab0695a737048ca67bf16948119cff2a7d8
heris <170120116115@git.org.in>
2021-12-16 18:28:02 +0530

    login register demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Dashboard from './components/Dashboard';
+import Alert from './components/layout/Alert';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Alert />
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/dashboard' component={Dashboard} />
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
